399c970d08a75bee06c2d3ef4cbd3dae96373415
David Weber <davidlweber@gmail.com>
2015-02-11 19:07:42 -0700

    setting up

---
+++ src/deque/Deque.java
@@ -0,0 +1,45 @@
+package deque;
+
+import java.util.Iterator;
+
+
+public class Deque {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+
+	
+	public Deque(){
+		// construct an empty deque
+	}
+	
+	public boolean isEmpty(){
+		return false;
+		// is the deque empty?
+	}
+	public int size() {
+		return 0;
+		// return the number of items on the deque
+	}
+	public void addFirst(Item item){
+		// insert the item at the front
+	}
+	public void addLast(Item item) {
+		// insert the item at the end
+	}
+	public Item removeFirst(){
+		return null;
+		// delete and return the item at the front
+	}
+	public Item removeLast() {
+		return null;
+		// delete and return the item at the end
+	}
+	public Iterator<Item> iterator() {
+		return null;
+		// return an iterator over items in order from front to end
+	}
+	
+}
