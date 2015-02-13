c298fd65bb07f76f39cff3050d636e5ae1c9b40d
compynerds <compynerds@users.noreply.github.com>
2015-02-12 21:01:44 -0700

    Update Deque.java

--- src/deque/Deque.java
+++ src/deque/Deque.java
@@ -3,43 +3,64 @@ package deque;
 import java.util.Iterator;
 
 
-public class Deque {
+public class Deque<Item> implements Iterable<Item> 
+{
+	private Node first;
+	private Node last;
+	private Node hold;
+	private Item holdItem;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	}
-
-
-	
-	public Deque(){
-		// construct an empty deque
-	}
-	
-	public boolean isEmpty(){
-		return false;
-		// is the deque empty?
-	}
-	public int size() {
-		return 0;
-		// return the number of items on the deque
-	}
-	public void addFirst(Item item){
-		// insert the item at the front
-	}
-	public void addLast(Item item) {
-		// insert the item at the end
-	}
-	public Item removeFirst(){
-		return null;
-		// delete and return the item at the front
-	}
-	public Item removeLast() {
-		return null;
-		// delete and return the item at the end
-	}
-	public Iterator<Item> iterator() {
-		return null;
-		// return an iterator over items in order from front to end
+	private class Node
+	{
+		private Item item;
+		private Node next;
 	}
 	
-}
+	   public Deque()      // construct an empty deque
+	   {
+		   first = new Node();
+		   last = first;
+	   }                     
+	   public boolean isEmpty()      // is the deque empty?
+	   {
+		   return (first == null)?true:false;
+	   }           
+	   public int size() // return the number of items on the deque
+	   {
+		   int elements = 0;
+		   //call iterator to go through each element in the list and add one to the size
+		   return elements;
+	   }                       
+	   public void addFirst(Item item)// insert the item at the front
+	   {
+		   hold = first;
+		   first = new Node();
+		   first.next = hold;		   
+	   }          
+	   public void addLast(Item item)// insert the item at the end
+	   {
+		   hold = last;
+		   last = new Node();
+		   hold.next = last;
+	   }           
+	   public Item removeFirst()// delete and return the item at the front
+	   {
+		   holdItem = first.item;
+		   first = first.next;
+		   return holdItem;
+	   }                
+	   public Item removeLast()// delete and return the item at the end
+	   {
+		   holdItem = last.item;
+		   //reset last to second to last item in list
+		   return holdItem;
+	   }                 
+	   public Iterator<Item> iterator()// return an iterator over items in order from front to end
+	   {
+		   //call a static class that you can code the methods for the hasNext(), remove(throw exception instead of not including it), next()
+		   return ;
+	   }
+	   public static void main(String[] args)
+	   {
+		   
+	   }   
