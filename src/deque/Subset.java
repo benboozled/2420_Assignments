5810dbf0f469e042ee5bcd145fb51e85357687e3
David Weber <davidlweber@gmail.com>
2015-02-14 11:29:41 -0700

    Trucking along

---
+++ src/deque/Subset.java
@@ -0,0 +1,15 @@
+package deque;
+
+import edu.princeton.cs.introcs.StdIn;
+
+public class Subset {
+	
+	public static void main(String[] args) {
+		
+		int k = StdIn.readInt();
+		
+		StdIn.readString();
+
+	}
+
+}
