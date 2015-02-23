ff380c86a559c30b3e4f6828ad3cb07bc4af0455
David Weber <davidlweber@gmail.com>
2015-02-23 16:25:55 -0700

    Code stubs

---
+++ src/autocomplete/BinarySearchDeluxe.java
@@ -0,0 +1,25 @@
+package autocomplete;
+
+import java.util.Comparator;
+
+public class BinarySearchDeluxe {
+
+	 // Return the index of the first key in a[] that equals the search key, or -1 if no such key.
+    public static <Key> int firstIndexOf(Key[] a, Key key, Comparator<Key> comparator){
+		return 0;
+    	
+    }
+
+    // Return the index of the last key in a[] that equals the search key, or -1 if no such key.
+    public static <Key> int lastIndexOf(Key[] a, Key key, Comparator<Key> comparator){
+		return 0;
+    	
+    }
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/autocomplete/Term.java
@@ -0,0 +1,10 @@
+package autocomplete;
+
+public class Term {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
