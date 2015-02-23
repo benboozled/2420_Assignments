1fba2280a100df9f3cc4e7359793a0375714cd96
David Weber <davidlweber@gmail.com>
2015-02-23 16:32:32 -0700

    code stub update

--- src/autocomplete/Term.java
+++ src/autocomplete/Term.java
@@ -1,10 +1,39 @@
 package autocomplete;
 
-public class Term {
+import java.util.Comparator;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
+public class Term implements Comparable<Term> {
+
+    // Initialize a term with the given query string and weight.
+    public Term(String query, double weight){
+    
+    }
+
+    // Compare the terms in descending order by weight.
+    public static Comparator<Term> byReverseWeightOrder(){
+		return null;
+    	
+    }
+
+    // Compare the terms in lexicographic order but using only the first r characters of each query.
+    public static Comparator<Term> byPrefixOrder(int r){
+		return null;
+    	
+    }
+
+    // Compare the terms in lexicographic order by query.
+    @Override
+    public int compareTo(Term that){
+		return 0;
+    	
+    }
+
+    // Return a string representation of the term in the following format:
+    // the weight, followed by a tab, followed by the query.
+    public String toString(){
+		return null;
+    	
+    }
 
 }
