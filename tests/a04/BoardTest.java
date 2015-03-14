e10e6e98539ed8eef1c7820fabd002b89c367f43
David Weber <davidlweber@gmail.com>
2015-03-13 20:56:40 -0600

    more of the same

--- tests/a04/BoardTest.java
+++ tests/a04/BoardTest.java
@@ -7,15 +7,24 @@ import org.junit.Test;
 
 import edu.princeton.cs.introcs.StdOut;
 
+@SuppressWarnings("unused")
 public class BoardTest {
 	
+	//weird boards
 	Board testBoard00;														//null
-	Board testBoardN201 = new Board(new int[][]{{1,2},{3,0}});				//N2
+	Board testBoardN201 = new Board(new int[][]{{1,2},{3,0}});				//2x2
+	//functional boards
 	Board testBoardN301 = new Board(new int[][]{{1,2,3},{4,5,6},{7,8,0}});	//basic board
 	Board testBoardN302 = new Board(new int[][]{{1,2,3},{4,5,6},{7,8,0}});	//same block positions as above
 	Board testBoardN303 = new Board(new int[][]{{1,2,3},{4,5,6},{7,0,8}});	//inverted blocks, unsolvable
 	Board testBoardN304 = new Board(new int[][]{{1,2,3},{4,0,5},{6,7,8}});	//center block open
 
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testBoardConstructor() {
+		Board testBoardN2401 = new Board(new int[][]{{1,2,3,4},{5,6,7,0}});		//2x4
+	}
+	
 	@Test
 	public void testSize() {
 		assertEquals(true, testBoardN301.size()==3);
@@ -31,15 +40,15 @@ public class BoardTest {
 	@Test
 	public void testToString() {
 		String testBoardN201string = "2"+"\n"+" 1  2 "+"\n"+" 3  0 "+"\n";
-		StdOut.println("testBoardN201.toString: \n"+testBoardN201.toString());
-		StdOut.println("testBoardN201string: \n"+testBoardN201string);
 		assertEquals(true, testBoardN201.toString().contentEquals(testBoardN201string));
+//		StdOut.println("testBoardN201.toString: \n"+testBoardN201.toString());
+//		StdOut.println("testBoardN201string: \n"+testBoardN201string);
 	}
 
-	@Test
-	public void testIsGoal() {
-		fail("Not yet implemented");
-	}
+//	@Test
+//	public void testIsGoal() {
+//		fail("Not yet implemented");
+//	}
 	
 	
 	/*
