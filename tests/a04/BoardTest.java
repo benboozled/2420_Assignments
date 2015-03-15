ecd40e37706e5cb48582577fc0d52837b4756602
David Weber <davidlweber@gmail.com>
2015-03-15 15:36:21 -0600

    no comment

--- tests/a04/BoardTest.java
+++ tests/a04/BoardTest.java
@@ -3,23 +3,63 @@ package a04;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import autocomplete.Autocomplete;
+import autocomplete.Term;
+import autocomplete.TermTest;
+import edu.princeton.cs.introcs.BinaryIn;
+import edu.princeton.cs.introcs.In;
+import edu.princeton.cs.introcs.StdArrayIO;
+import edu.princeton.cs.introcs.StdIn;
 import edu.princeton.cs.introcs.StdOut;
 
 @SuppressWarnings("unused")
 public class BoardTest {
 	
+	private static String file = "/A04Tests/puzzle10.txt";
+	
+	private Board boardReader(String inputFile){
+		In in = new In(inputFile);
+        int N = in.readInt();
+        int[][] boardFile = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+            	boardFile[i][j] = in.readInt();
+            }
+        }
+		return new Board(boardFile);
+	}
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
 	//weird boards
 	Board testBoard00;														//null
 	Board testBoardN222 = new Board(new int[][]{{1,2},{3,0}});				//2x2
 	Board testBoardN323u = new Board(new int[][]{{1,2,3},{4,5,6},{7,0,8}});	//inverted blocks, unsolvable
 	Board testBoardN333b = new Board(new int[][]{{1,2,3},{4,5,6},{7,8,0}});	//same block positions N301
+	//board from file
+	Board fileBoard = boardReader(file);									//board from file specified above
 	//functional boards 
 	Board testBoardN333 = new Board(new int[][]{{1,2,3},{4,5,6},{7,8,0}});	//basic solved board
 	//unsolved
 	Board testBoardN322 = new Board(new int[][]{{1,2,3},{4,0,5},{6,7,8}});	//2,2 block open;6 manhattan;4 ham
 	Board testBoardN311 = new Board(new int[][]{{0,1,2},{5,3,4},{6,7,8}});	//1,1 block open;12 manhattan;8 ham
+	
+	@Test
+	public void testBoardReader(){
+		StdOut.println(file);
+		StdOut.print(boardReader(file).toString());
+		StdOut.println("hamming: "+fileBoard.hamming());
+		StdOut.println("manhattan: "+fileBoard.manhattan());
+	}
 	
 	@Test
 	public void testManhattan() {
