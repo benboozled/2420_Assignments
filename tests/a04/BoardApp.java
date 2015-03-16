8a9a6e919a482dd3a538aed4fb36a689641a91d7
David Weber <davidlweber@gmail.com>
2015-03-16 00:35:19 -0600

    no comment

--- tests/a04/BoardApp.java
+++ tests/a04/BoardApp.java
@@ -18,26 +18,26 @@ public class BoardApp {
 	}
 	
 	public static void traceBoard(Board brd){
-		long startTime = System.currentTimeMillis();
-		StdOut.print(brd.toString());
-		StdOut.println("hamming: "+brd.hamming());
-		StdOut.println("manhattan: "+brd.manhattan());
-		long endTime   = System.currentTimeMillis();
-		long totalTime = endTime - startTime;
-		System.out.println("runtime: "+totalTime/1000.0);
+		long startTime = System.currentTimeMillis();			//get the system time at start of method
+		StdOut.print(brd.toString());							//print the board
+		StdOut.println("hamming: "+brd.hamming());				//print the hamming result
+		StdOut.println("manhattan: "+brd.manhattan());			//print the manhattan result
+		long endTime   = System.currentTimeMillis();			//get the system time at end of method
+		long totalTime = endTime - startTime;					//difference between start and end
+		System.out.println("runtime: "+totalTime/1000.0);		//print time to perform hamming, manahattan															
 		StdOut.println("---------------------------------");
 	}
 
 	public static Board readBoard(String inputFile){
-		In in = new In(inputFile);
-		int N = in.readInt();
-		int[][] boardFile = new int[N][N];
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				boardFile[i][j] = in.readInt();
+		In in = new In(inputFile);					//Create new In Object for puzzle txt file
+		int N = in.readInt();						//Make the first line the board dimensions
+		int[][] boardFile = new int[N][N];			//Declare 2D array
+		for (int i = 0; i < N; i++) {				//for each row...
+			for (int j = 0; j < N; j++) {			//for each column...
+				boardFile[i][j] = in.readInt();		//add the next int from the file
 			}
 		}
-		return new Board(boardFile);
+		return new Board(boardFile);				//return a new board made with the 2D array 
 	}
 
 }
