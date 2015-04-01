71969c86ccf026206f5551fc1e21d8e24f7eacf9
David Weber <davidlweber@gmail.com>
2015-03-31 22:26:50 -0600

    no comment

--- src/kdTree/PointST.java
+++ src/kdTree/PointST.java
@@ -1,53 +1,53 @@
 package kdTree;
 
+import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.RedBlackBST;
 
 public class PointST<Value> {
 	
 	RedBlackBST<Point2D, Value> points = new RedBlackBST<>();
 	
-	public PointST() { // construct an empty symbol table of points
+	public PointST() { 							// construct an empty symbol table of points
 		 
 	}
 	
-	public boolean isEmpty(){// is the symbol table empty?
+	public boolean isEmpty(){					// is the symbol table empty?
 		return points.isEmpty();  
 	}
 	
-	public int size(){// number of points
+	public int size(){							// number of points
 		return points.size();  
 	}
 	
-	public void put(Point2D p, Value val){// associate the value val with point p
+	public void put(Point2D p, Value val){		// associate the value val with point p
 		points.put(p, val);
 	}
 	
-	public Value get(Point2D p){// value associated with point p
+	public Value get(Point2D p){				//TODO: value associated with point p
 		
 		return null;
 	}
 	
-	public boolean contains(Point2D p){// does the symbol table contain point p?
+	public boolean contains(Point2D p){			//TODO: does the symbol table contain point p?
 		
 		return false;
 	}
 	
-	public Iterable<Point2D> points(){// all points in the symbol table 
-		
-		return null;  
+	public Iterable<Point2D> points(){			// all points in the symbol table 
+		return points.keys();  
 	}
 	
-	public Iterable<Point2D> range(RectHV rect){// all points that are inside the rectangle 
-		
+	public Iterable<Point2D> range(RectHV rect){//TODO: all points that are inside the rectangle 
+
 		return null; 
 	}
 	
-	public Point2D nearest(Point2D p){// a nearest neighbor to point p; null if the symbol table is empty
+	public Point2D nearest(Point2D p){			//TODO: a nearest neighbor to point p; null if the symbol table is empty
 		
 		return p; 
 	}
 	
-	public static void main(String[] args){// unit testing of the methods (not graded)
+	public static void main(String[] args){		// unit testing of the methods (not graded)
 		
 	}
 	
