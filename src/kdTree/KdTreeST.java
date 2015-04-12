b85c58e659bb54a0bf269e62bfdd88f96b197470
David Weber <davidlweber@gmail.com>
2015-04-12 16:36:27 -0600

    no comment

--- src/kdTree/KdTreeST.java
+++ src/kdTree/KdTreeST.java
@@ -3,32 +3,29 @@ package kdTree;
 import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.introcs.StdOut;
 
-@SuppressWarnings("unused")
+
 public class KdTreeST<Value> {
-			
+
 	private Node root;
 	private int N;
 	
-	private double minX = Double.MIN_VALUE;
-	private double minY = Double.MIN_VALUE;
-	private double maxX = Double.MAX_VALUE;
-	private double maxY = Double.MAX_VALUE;
-
+//	private double minX = Double.MIN_VALUE;
+//	private double minY = Double.MIN_VALUE;
+//	private double maxX = Double.MAX_VALUE;
+//	private double maxY = Double.MAX_VALUE;
+//	private RectHV infinity = new RectHV(minX, minY, maxX, maxY );
+	
 	private class Node {
-		   private Point2D point;		// the point
-		   private Value value;			// the symbol table maps the point to this value
-		   private RectHV rect;			// the axis-aligned rectangle corresponding to this node
-		   private Node leftBottom;		// the left/bottom subtree
-		   private Node rightTop;		// the right/top subtree
-		   private Node parent;			// the node above
-		   private int level;			// the tree level the node is on
-
-		   public Node(Point2D point, Value value, Node parent, int level){
-			   this.point = point;
-			   this.value = value;
-			   this.parent = parent;
-			   this.level = level;
-		   }
+		private Point2D point;		// the point
+		private Value value;		// the symbol table maps the point to this value
+		private Node leftBottom;	// the left/bottom subtree
+		private Node rightTop;		// the right/top subtree
+		private int level;			// the tree level the node is on
+		public Node(Point2D point, Value value, int level){
+			this.point = point;
+			this.value = value;
+			this.level = level;
+		}  
 	}
 
 	public KdTreeST() {
@@ -60,37 +57,31 @@ public class KdTreeST<Value> {
 	 */
 	public void put(Point2D point, Value val){
 		if (val == null)	throw new IllegalArgumentException ("Unable to insert null value into KDTree");
-		RectHV infinity = new RectHV(Double.MIN_VALUE, Double.MIN_VALUE, Double.MAX_VALUE, Double.MAX_VALUE );
-		root = put(	root, null, point, infinity, val, 0);
+		root = put(root, point, val, 0);
 		N++;
 	}
 	
-	private Node put(Node node, Node parent, Point2D point, RectHV rect, Value val, int level) {
-		if (node == null)	return new Node(point, val, node, level+1);
+	private Node put(Node node, Point2D point, Value val, int level) {
+		if (node == null) 	return new Node(point, val, level+1);
 		if (node.level % 2 == 1){
 			if (point.x()-node.point.x() <  0)	
-				node.leftBottom = put(node.leftBottom, node, point, rect, val, level+1);
+				node.leftBottom = put(node.leftBottom, point, val, level+1);
 			if (point.x()-node.point.x() >= 0)	
-				node.rightTop   = put(node.rightTop,   node, point, rect, val, level+1);
-			if (point.x() > minX)			minX = node.point.x();
-			if (point.x() < minX)			minX = Double.MIN_VALUE;
-			if (point.x() < maxX)			maxX = node.point.x();
-			if (point.x() > maxX)			maxX = Double.MAX_VALUE;
+				node.rightTop   = put(node.rightTop,   point, val, level+1);
+//			minX = point.x() <  minX? node.point.x() : Double.MIN_VALUE;
+//			maxX = point.x() >= maxX? Double.MIN_VALUE : node.point.x();			
 		}
 		if (node.level % 2 == 0){
 			if (point.y()-node.point.y() <  0)
-				node.leftBottom = put(node.leftBottom, node, point, rect, val, level+1);		
+				node.leftBottom = put(node.leftBottom, point, val, level+1);		
 			if (point.y()-node.point.y() >= 0)
-				node.rightTop   = put(node.rightTop,   node, point, rect, val, level+1);
-			
-			if (point.y() > minY)			minY = node.point.y();
-			if (point.y() < minY)			minY = Double.MIN_VALUE;
-			if (point.y() < maxY)			maxY = node.point.y();
-			if (point.y() > maxY)			maxY = Double.MAX_VALUE;
+				node.rightTop   = put(node.rightTop,   point, val, level+1);
+//			minY = point.y() <  minY? node.point.y() : Double.MIN_VALUE;
+//			maxY = point.y() >= maxY? Double.MIN_VALUE : node.point.y();				
 		}
 		return node;
 	}
-	
+
 	/**
 	* returns the value associated with a given point
 	* @param a point of type Point2D
@@ -127,42 +118,18 @@ public class KdTreeST<Value> {
 	}
 
     public Iterable<Point2D> points() {
-        return points(min(), max());
+        Queue<Point2D> points = new Queue<Point2D>();
+        Queue<Node> queue = new Queue<Node>();
+        queue.enqueue(root);
+        while (!queue.isEmpty()) {
+            Node x = queue.dequeue();
+            if (x == null) continue;
+            points.enqueue(x.point);
+            queue.enqueue(x.leftBottom);
+            queue.enqueue(x.rightTop);
+        }
+        return points;
     }
-    
-	/**
-	 * Return all of the points
-	 * @return all of the points
-	 */
-	private Iterable<Point2D> points(Point2D lo, Point2D hi){
-		Queue<Point2D> queue = new Queue<Point2D>();
-        points(root, queue, lo, hi);
-        return queue;
-	}
-	 
-    private void points(Node node, Queue<Point2D> queue, Point2D lo, Point2D hi) { 
-        if (node == null) return; 
-      	queue.enqueue(node.point);
-       	points(node.leftBottom, queue, lo, hi); 
-       	points(node.rightTop, queue, lo, hi);  
-    } 
-
-	private Point2D min() {
-    	if (isEmpty()) return null;
-    	return min(root).point;
-    } 
-    private Node min(Node x) { 
-    	if (x.leftBottom == null) return x; 
-    	else                return min(x.leftBottom); 
-    } 
-    private Point2D max() {
-    	if (isEmpty()) return null;
-    	return max(root).point;
-    } 
-    private Node max(Node x) { 
-    	if (x.rightTop == null) return x; 
-    	else                 return max(x.rightTop); 
-    } 
     	
 	/**
 	 * returns a range of points that are contained within a given rectangle
@@ -171,8 +138,13 @@ public class KdTreeST<Value> {
 	 * @return all points that are inside the rectangle 
 	 */
 	public Iterable<Point2D> range(RectHV rect){//TODO: all points that are inside the rectangle 
-		
-		return null; 
+		Queue<Point2D> queue = new 	Queue<Point2D>();
+		// for each point that is returned points(), enqueue the point to a queue if it is contained in the rectangle
+        for (Point2D p: points()) {
+        	if (rect.contains(p))
+        		queue.enqueue(p);
+        }
+        return queue; 
 	}
 	
 	/**
@@ -180,16 +152,22 @@ public class KdTreeST<Value> {
 	 * @param a point of type Point2D
 	 * @return the nearest neighbor of a given point, null if the table is empty
 	 */
-	public Point2D nearest(Point2D p){//TODO: a nearest neighbor to point p; null if the symbol table is empty
+	public Point2D nearest(Point2D p) {               // a nearest neighbor in the set to p; null if set is empty
 		if (p == null) 
 			throw new NullPointerException("Null pointer exception");
+		double nearest = 1.5;
+        Point2D nearestP = null;
+        for (Point2D p2: points()) {
+        	if (p2.distanceSquaredTo(p) < nearest) {
+        		nearest = p2.distanceSquaredTo(p);
+        			nearestP = p2;
+            }            
+        }
+        return nearestP;
+	}
 		
-		return p; 
-	}
-	
 	public static void main(String[] args){  
-        
-	}
 
+	}
 	
 }
--- tests/kdTree/KdTreeSTTest.java
+++ tests/kdTree/KdTreeSTTest.java
@@ -2,6 +2,7 @@ package kdTree;
 
 import static org.junit.Assert.*;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import edu.princeton.cs.introcs.In;
@@ -13,10 +14,19 @@ public class KdTreeSTTest {
 	In inInput10 = new In("/kdTreeTests/input10.txt");
 	In inInputLevelOrder = new In("/kdTreeTests/inputLevelOrder.txt");
 
+	@Ignore
+	public void testRectangle() {		
+//		StdOut.print("\n------------------------RECTANGLES");
+//		StdOut.println("\n-----worksheet");
+//		KdTreeST.main(null);
+		//KdTreeST.testRectangle();
+        //assertEquals(true, KdTreeST.testRectangle());
+	}
+	
 	@Test
 	public void testContains() {
-		StdOut.print("\n------------------------CONTAINS");
-		StdOut.println("\n-----worksheet");
+		//StdOut.print("\n------------------------CONTAINS");
+		//StdOut.println("\n-----worksheet");
 		KdTreeST<Integer> kdTreeTestContains1 = new KdTreeST<Integer>();
         for (int i = 0; !inWorksheet.isEmpty(); i++) {
             double x = inWorksheet.readDouble();
@@ -24,29 +34,29 @@ public class KdTreeSTTest {
             Point2D p = new Point2D(x, y);
             kdTreeTestContains1.put(p, i);
         }
-        StdOut.print("\npoint 2,3: "+kdTreeTestContains1.contains(new Point2D(2,3)));
+        //StdOut.print("\npoint 2,3: "+kdTreeTestContains1.contains(new Point2D(2,3)));
         assertEquals(true, kdTreeTestContains1.contains(new Point2D(2,3)));
-        StdOut.print("\npoint 4,2: "+kdTreeTestContains1.contains(new Point2D(4,2)));
+        //StdOut.print("\npoint 4,2: "+kdTreeTestContains1.contains(new Point2D(4,2)));
         assertEquals(true, kdTreeTestContains1.contains(new Point2D(4,2)));
-        StdOut.print("\npoint 4,5: "+kdTreeTestContains1.contains(new Point2D(4,5)));
+        //StdOut.print("\npoint 4,5: "+kdTreeTestContains1.contains(new Point2D(4,5)));
 		assertEquals(true, kdTreeTestContains1.contains(new Point2D(4,5)));
-		StdOut.print("\npoint 3,3: "+kdTreeTestContains1.contains(new Point2D(3,3)));
+		//StdOut.print("\npoint 3,3: "+kdTreeTestContains1.contains(new Point2D(3,3)));
 		assertEquals(true, kdTreeTestContains1.contains(new Point2D(3,3)));
-		StdOut.print("\npoint 1,5: "+kdTreeTestContains1.contains(new Point2D(1,5)));
+		//StdOut.print("\npoint 1,5: "+kdTreeTestContains1.contains(new Point2D(1,5)));
 		assertEquals(true, kdTreeTestContains1.contains(new Point2D(1,5)));
-		StdOut.print("\npoint 4,4: "+kdTreeTestContains1.contains(new Point2D(4,4)));
+		//StdOut.print("\npoint 4,4: "+kdTreeTestContains1.contains(new Point2D(4,4)));
 		assertEquals(true, kdTreeTestContains1.contains(new Point2D(4,4)));
 		
-		StdOut.print("\npoint 0,0: "+kdTreeTestContains1.contains(new Point2D(0,0)));
+		//StdOut.print("\npoint 0,0: "+kdTreeTestContains1.contains(new Point2D(0,0)));
 		assertEquals(false, kdTreeTestContains1.contains(new Point2D(0,0)));
-		StdOut.print("\npoint 10,10: "+kdTreeTestContains1.contains(new Point2D(10,10)));
+		//StdOut.print("\npoint 10,10: "+kdTreeTestContains1.contains(new Point2D(10,10)));
 		assertEquals(false, kdTreeTestContains1.contains(new Point2D(10,10)));
 	}
 	
 	@Test
 	public void testGet() {
-		StdOut.print("\n------------------------GET");
-		StdOut.println("\n-----worksheet");
+		//StdOut.print("\n------------------------GET");
+		//StdOut.println("\n-----worksheet");
 		KdTreeST<Integer> kdTreeTestGet1 = new KdTreeST<Integer>();
         for (int i = 0; !inWorksheet.isEmpty(); i++) {
             double x = inWorksheet.readDouble();
@@ -54,144 +64,145 @@ public class KdTreeSTTest {
             Point2D p = new Point2D(x, y);
             kdTreeTestGet1.put(p, i);
         }
-        StdOut.print("\npoint 2,3: "+kdTreeTestGet1.get(new Point2D(2,3)));
+        //StdOut.print("\npoint 2,3: "+kdTreeTestGet1.get(new Point2D(2,3)));
         assertEquals(true, kdTreeTestGet1.get(new Point2D(2,3))==0);
-        StdOut.print("\npoint 4,2: "+kdTreeTestGet1.get(new Point2D(4,2)));
+        //StdOut.print("\npoint 4,2: "+kdTreeTestGet1.get(new Point2D(4,2)));
         assertEquals(true, kdTreeTestGet1.get(new Point2D(4,2))==1);
-        StdOut.print("\npoint 4,5: "+kdTreeTestGet1.get(new Point2D(4,5)));
+        //StdOut.print("\npoint 4,5: "+kdTreeTestGet1.get(new Point2D(4,5)));
 		assertEquals(true, kdTreeTestGet1.get(new Point2D(4,5))==2);
-		StdOut.print("\npoint 3,3: "+kdTreeTestGet1.get(new Point2D(3,3)));
+		//StdOut.print("\npoint 3,3: "+kdTreeTestGet1.get(new Point2D(3,3)));
 		assertEquals(true, kdTreeTestGet1.get(new Point2D(3,3))==3);
-		StdOut.print("\npoint 1,5: "+kdTreeTestGet1.get(new Point2D(1,5)));
+		//StdOut.print("\npoint 1,5: "+kdTreeTestGet1.get(new Point2D(1,5)));
 		assertEquals(true, kdTreeTestGet1.get(new Point2D(1,5))==4);
-		StdOut.print("\npoint 4,4: "+kdTreeTestGet1.get(new Point2D(4,4)));
-		StdOut.print(" <----------??\n");
+		//StdOut.print("\npoint 4,4: "+kdTreeTestGet1.get(new Point2D(4,4)));
+		//StdOut.print(" <----------??\n");
 		//assertEquals(true, kdTreeTestGet1.get(new Point2D(4,4))==5);
 	}
 
-//	@Test
-//	public void testPointsLevelOrder() {
-//		StdOut.println("\n----------------------POINTS");
-//		StdOut.println("-----levelOrder");
-//		KdTreeST<Integer> kdTreeTestPoints1 = new KdTreeST<Integer>();
-//        for (int i = 0; !inInputLevelOrder.isEmpty(); i++) {
-//            double x = inInputLevelOrder.readDouble();
-//            double y = inInputLevelOrder.readDouble();
-//            Point2D p = new Point2D(x, y);
-//            StdOut.print(p.toString());
-//            kdTreeTestPoints1.put(p, i);
-//        }
-//        StdOut.println();
-//		StringBuilder actual = new StringBuilder();
-//		String expected = "(0.7, 0.2)(0.5, 0.4)(0.9, 0.6)(0.2, 0.3)(0.4, 0.7)";
-//		for (Point2D pnt : kdTreeTestPoints1.points()){
-//			actual.append(pnt.toString());
-//		}
-//		assertEquals(expected, actual.toString());
-//	}
+	@Test
+	public void testPointsLevelOrder() {
+		//StdOut.println("\n----------------------POINTS");
+		//StdOut.println("-----testPointsLevelOrder,worksheet");
+		KdTreeST<Integer> kdTreeTestPoints1 = new KdTreeST<Integer>();
+        for (int i = 0; !inInputLevelOrder.isEmpty(); i++) {
+            double x = inInputLevelOrder.readDouble();
+            double y = inInputLevelOrder.readDouble();
+            Point2D p = new Point2D(x, y);
+            kdTreeTestPoints1.put(p, i);
+        }
+		StringBuilder actual = new StringBuilder();
+		String expected = "(0.7, 0.2)(0.5, 0.4)(0.9, 0.6)(0.2, 0.3)(0.4, 0.7)";
+		//StdOut.println();
+		for (Point2D pnt : kdTreeTestPoints1.points()){
+			//StdOut.print(pnt.toString());
+			actual.append(pnt.toString());
+		}
+		//StdOut.println();
+		//StdOut.print(expected+"\n");
+		assertEquals(expected, actual.toString());
+	}
 
 	@Test
 	public void testPoints2() {
-		StdOut.println("\n\n-----worksheet");
+		//StdOut.println("\n\n-----testPoints2,worksheet");
 		KdTreeST<Integer> kdTreeTestPoints2 = new KdTreeST<Integer>();
         for (int i = 0; !inWorksheet.isEmpty(); i++) {
             double x = inWorksheet.readDouble();
             double y = inWorksheet.readDouble();
             Point2D p = new Point2D(x, y);
-            StdOut.print(p.toString());
+            
             kdTreeTestPoints2.put(p, i);
         }
-        StdOut.println();
+        //StdOut.println();
 		StringBuilder actual2 = new StringBuilder();
 		String expected2 = "(2.0, 3.0)(1.0, 5.0)(4.0, 2.0)(4.0, 5.0)(3.0, 3.0)(4.0, 4.0)";
 		for (Point2D pnt : kdTreeTestPoints2.points()){
+			//StdOut.print(pnt.toString());
 			actual2.append(pnt.toString());
 		}
+		//StdOut.println();
+		//StdOut.print(expected2+"\n");
 		assertEquals(expected2, actual2.toString());
 	}	
 	
 	@Test
 	public void testPut() {
-		StdOut.print("\n------------------------PUT");
-		StdOut.println("\n-----worksheet");
+		//StdOut.print("\n------------------------PUT");
+		//StdOut.println("\n-----worksheet");
 		KdTreeST<Integer> kdTreeTestPut1 = new KdTreeST<Integer>();
         for (int i = 0; !inWorksheet.isEmpty(); i++) {
             double x = inWorksheet.readDouble();
             double y = inWorksheet.readDouble();
             Point2D p = new Point2D(x, y);
-            StdOut.print(p.toString());
+            //StdOut.print(p.toString());
             kdTreeTestPut1.put(p, i);
         }
 //--------------------------------------------------------------------------
-        StdOut.println("\n-----input10");
-		KdTreeST<Integer> kdTreeTestPut2 = new KdTreeST<Integer>();
-        for (int i = 0; !inInput10.isEmpty(); i++) {
-            double x = inInput10.readDouble();
-            double y = inInput10.readDouble();
-            Point2D p = new Point2D(x, y);
-            StdOut.println("adding "+p.toString());
-            kdTreeTestPut2.put(p, i);
-        }
+//        //StdOut.println("\n-----input10");
+//		KdTreeST<Integer> kdTreeTestPut2 = new KdTreeST<Integer>();
+//        for (int i = 0; !inInput10.isEmpty(); i++) {
+//            double x = inInput10.readDouble();
+//            double y = inInput10.readDouble();
+//            Point2D p = new Point2D(x, y);
+//            //StdOut.println("adding "+p.toString());
+//            kdTreeTestPut2.put(p, i);
+//        }
 	}
 	
 	@Test
 	public void testIsEmpty() {
-		StdOut.println("\n\n----------------------ISEMPTY");
+		//StdOut.println("\n\n----------------------ISEMPTY");
 		KdTreeST<Integer> testIsEmpty = new KdTreeST<Integer>();
 		assertEquals(true, testIsEmpty.isEmpty());
-		StdOut.println("isEmpty: "+(testIsEmpty.isEmpty()?"true":"false"));
+		//StdOut.println("isEmpty: "+(testIsEmpty.isEmpty()?"true":"false"));
 		
 		testIsEmpty.put(new Point2D(2, 3), 0);
-		StdOut.println("adding 2,3");
+		//StdOut.println("adding 2,3");
 		testIsEmpty.put(new Point2D(4, 5), 1);
-		StdOut.println("adding 4,5");
+		//StdOut.println("adding 4,5");
 		testIsEmpty.put(new Point2D(6, 7), 2);
-		StdOut.println("adding 6,7");
+		//StdOut.println("adding 6,7");
 		assertEquals(false, testIsEmpty.isEmpty());
-		StdOut.println("isEmpty: "+(testIsEmpty.isEmpty()?"true":"false"));
+		//StdOut.println("isEmpty: "+(testIsEmpty.isEmpty()?"true":"false"));
 	}
 	
-	/*	
-	@Test
+	@Ignore
+	public static void traceRect(RectHV rect){
+		StdOut.print("\n[(");
+		StdOut.print(rect.xmin() == Double.MIN_VALUE? "-inf": rect.xmin());
+		StdOut.print(", ");
+		StdOut.print(rect.ymin() == Double.MIN_VALUE? "-inf": rect.ymin());
+		StdOut.print(")(");
+		StdOut.print(rect.xmax() == Double.MAX_VALUE? "+inf": rect.xmax());
+		StdOut.print(", ");
+		StdOut.print(rect.ymax() == Double.MAX_VALUE? "+inf": rect.ymax());
+		StdOut.print(")]");
+	}
+
+	@Ignore
 	public void testSize() {
-StdOut.println("\n----------testSize------------");
-		KdTreeST<Point2D> kDTest03 = new KdTreeST<Point2D>();
-		assertEquals(0, kDTest03.size());
-		
-		kDTest03.put(testPoint23, testPoint23);
-//		assertEquals(1, kDTest03.size());
-		
-		kDTest03.put(testPoint23, testPoint23);
-		kDTest03.put(testPoint42, testPoint42);
-		assertEquals(3, kDTest03.size());
-	}
-	
-	
-
-	@Test
-	public void testGet() {
-	//        StdOut.println("2,3 parent is: "+kdTreeTestPut1.get(new Point2D(2,3)));
-//        StdOut.println("4,5 parent is: "+kdTreeTestPut1.get(new Point2D(4,5)));
-		fail("Not yet implemented");
+//		StdOut.println("\n----------testSize------------");
+//		KdTreeST<Point2D> kDTest03 = new KdTreeST<Point2D>();
+//		assertEquals(0, kDTest03.size());
+//		
+//		kDTest03.put(testPoint23, testPoint23);
+////		assertEquals(1, kDTest03.size());
+//		
+//		kDTest03.put(testPoint23, testPoint23);
+//		kDTest03.put(testPoint42, testPoint42);
+//		assertEquals(3, kDTest03.size());
 	}
 
-	@Test
-	public void testContains() {
-		fail("Not yet implemented");
-	}
-
-
-
-	@Test
+	@Ignore
 	public void testRange() {
 		fail("Not yet implemented");
 	}
 
-	@Test
+	@Ignore
 	public void testNearest() {
 		fail("Not yet implemented");
 	}
-	*/
+
 }
 
 /*-------------------CODE SCRAPHEAP----------------------------------*/
@@ -267,4 +278,93 @@ StdOut.println("\n----------testSize------------");
 //	if (node.orientation == Oriented.VERTICALLY) orent = " | ";
 //	if (node.orientation == Oriented.HORIZONTALLY) orent = "---";
 //	return String.format("%-20s %20s\n", node.point.toString(), orent);
+//}
+
+//double xnew = (point.x() >= rect.xmin()? point.x() : Double.MIN_VALUE);	   	
+//private RectHV bisect(RectHV rect, Point2D point, int level){
+//	/*TODO: delete trace*/StdOut.print("\n"+point);
+//	if (level % 2 == 1){//x
+//		if (point.x() > rect.xmin())
+//			return new RectHV(point.x(),rect.ymin(),rect.xmax(),rect.ymax());
+//		/*TODO: delete trace*/KdTreeSTTest.traceRect(rect);
+//		if (point.x()  < rect.xmin())
+//			return new RectHV(Double.MIN_VALUE,rect.ymin(),rect.xmax(),rect.ymax());
+//		/*TODO: delete trace*/KdTreeSTTest.traceRect(rect);
+//		if (point.x()  < rect.xmax())
+//			new RectHV(rect.xmin(),rect.ymin(),point.y(),rect.ymax());
+//		/*TODO: delete trace*/KdTreeSTTest.traceRect(rect);
+//		if (point.x()  > rect.xmax())
+//			new RectHV(rect.xmin(),rect.ymin(),Double.MAX_VALUE,rect.ymax());
+//		/*TODO: delete trace*/KdTreeSTTest.traceRect(rect);
+//	}
+//	if (level % 2 == 0){//y
+//		if (point.y() > rect.ymin())
+//			return new RectHV(point.x(),rect.ymin(),rect.xmax(),rect.ymax());
+//		/*TODO: delete trace*/KdTreeSTTest.traceRect(rect);
+//		if (point.y() < rect.ymin())
+//			return new RectHV(Double.MIN_VALUE,rect.ymin(),rect.xmax(),rect.ymax());
+//		/*TODO: delete trace*/KdTreeSTTest.traceRect(rect);
+//		if (point.y() < rect.ymax())
+//			new RectHV(rect.xmin(),rect.ymin(),point.y(),rect.ymax());
+//		/*TODO: delete trace*/KdTreeSTTest.traceRect(rect);
+//		if (point.y() > rect.ymax())
+//			new RectHV(rect.xmin(),rect.ymin(),Double.MAX_VALUE,rect.ymax());
+//		/*TODO: delete trace*/KdTreeSTTest.traceRect(rect);
+//	}
+//	System.err.println("\nunhandled rectangle condition");
+	
+	
+//		if (point.x() > minX)			minX = node.point.x();
+//		if (point.x() < minX)			minX = Double.MIN_VALUE;
+//		if (point.x() < maxX)			maxX = node.point.x();
+//		if (point.x() > maxX) maxX = Double.MAX_VALUE;
+	
+//		if (point.y() > minY)			minY = node.point.y();
+//		if (point.y() < minY)			minY = Double.MIN_VALUE;
+//		if (point.y() < maxY)			maxY = node.point.y();
+//		if (point.y() > maxY) maxY = Double.MAX_VALUE;
+//	if (level % 2 == 1){//y
+//		if (point > rect.ymin())
+//
+//		if (point < rect.ymin())
+//
+//		if (point < rect.ymax())
+//
+//		if (point > rect.ymax())
+//			;
+//	}
+//	
+//	
+//	new RectHV(rect.xmin(),point.y(),rect.xmax(),rect.ymax());
+//	new RectHV(rect.xmin(),rect.ymin(),rect.xmax(),point.y());		
+//	return infinity;
+//}
+//private Node put(Node node, Point2D point, Value val, int level, RectHV rect) {
+//if (node == null)	return new Node(point, val, level+1,
+//					new RectHV(rect.xmin(),rect.ymin(),rect.xmax(),rect.ymax()));
+//
+///*TODO: delete trace*/StdOut.print("\n-------------"+point);
+//if (node.level % 2 == 1){										//for VERTICAL line 			
+//	if (point.x()-node.point.x() <  0){							//new point LEFT of old point
+//		node.leftBottom = put(	node.leftBottom, 				//put node in LEFT branch 
+//								point, val, 					//recurse
+//								level+1, 						//increment LEVEL
+//								new RectHV(Double.MIN_VALUE,	//new XMIN
+//								rect.ymin(),rect.xmax(),rect.ymax()));//recurse
+//		/*TODO: delete trace*/KdTreeSTTest.traceRect(node.rect);
+//	}
+//	if (point.x()-node.point.x() >= 0)	{
+//		node.rightTop = put(node.rightTop, point, val, level+1, node.rect);
+//		///*TODO: delete trace*/KdTreeSTTest.traceRect(rect);
+//	}
+//}
+//if (node.level % 2 == 0){//horizontal line, upper right corner
+//	if (point.y()-node.point.y() <  0)
+//		node.leftBottom = put(node.leftBottom, point, val, level+1, node.rect);
+//	///*TODO: delete trace*/KdTreeSTTest.traceRect(rect);
+//	if (point.y()-node.point.y() >= 0)
+//		node.rightTop = put(node.rightTop, point, val, level+1, node.rect);
+//	///*TODO: delete trace*/KdTreeSTTest.traceRect(rect);
+//}
+//return node;
 //}
