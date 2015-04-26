e69ced40b988e8406158d09a1b68919ce6640d06
David Weber <davidlweber@gmail.com>
2015-04-26 12:50:15 -0600

    no comment

--- src/synsets/SAP.java
+++ src/synsets/SAP.java
@@ -1,59 +1,176 @@
 package synsets;
 
+import java.util.Set;
+
+import edu.princeton.cs.algs4.BreadthFirstDirectedPaths;
 import edu.princeton.cs.algs4.Digraph;
-import edu.princeton.cs.algs4.DirectedCycle;
+import edu.princeton.cs.algs4.Graph;
+import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.Topological;
+import edu.princeton.cs.introcs.StdOut;
 
 public class SAP {
-	
-		private final Digraph graph;
-		private Stack<Integer> cycle; // vertices on a cycle (if one exists)
-		private boolean[] onStack; // vertices on recursive call stack
-			
-		// constructor takes a digraph (not necessarily a DAG)
-		public SAP(Digraph G){
-			this.graph = G;
-		}
-		
-		// is the digraph a directed acyclic graph?
-		public boolean isDAG(){
-			DirectedCycle myDiCycle = new DirectedCycle(graph);
-			return true == myDiCycle.hasCycle();
-		}
-		
-		// is the digraph a rooted DAG?
-		public boolean isRootedDAG(){
-			
-			return false;
-		}
-		
-		// length of shortest ancestral path between v and w; -1 if no such path
-		public int length(int v, int w){
-			
-			return w;
-		}
-		
-		// a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
-		public int ancestor(int v, int w){
-			
-			return w;
-		}
-		
-		// length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
-		public int length(Iterable<Integer> v, Iterable<Integer> w){
-			
-			return 0;
-		}
-		
-		// a common ancestor that participates in shortest ancestral path; -1 if no such path
-		public int ancestor(Iterable<Integer> v, Iterable<Integer> w){
-			
-			return 0;
-		}
-		
-		// do unit testing of this class
-		public static void main(String[] args){
-			
-		}
-	
+
+	//TODO: deprecate topo;
+	private Topological topo;
+	private final Digraph digraph;
+    
+	/**
+	 *  constructor takes a digraph (not necessarily a DAG)
+	 * @param digraph
+	 */
+	public SAP(Digraph graph){
+		this.digraph = graph;
+		topo = new Topological(graph);
 	}
+	
+	/**
+	 *  is the digraph a directed acyclic graph?
+	 * @return
+	 */
+	public boolean isDAG(){
+		return topo.order() !=null;
+	}
+	
+	/**TODO:
+	 *  is the digraph a rooted DAG?
+	 * @return
+	 */
+	public boolean isRootedDAG(){
+		return false;
+	}
+	
+	/**TODO
+	 *  length of shortest ancestral path between v and w; -1 if no such path
+	 * @param v
+	 * @param w
+	 * @return
+	 */
+	public int length(int v, int w){
+		
+		return w;
+	}
+	
+	/**TODO
+	 *  a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
+	 * @param v
+	 * @param w
+	 * @return
+	 */
+	public int ancestor(int v, int w){
+	//public int ancestor(Iterable<Integer> v, Iterable<Integer> w){
+		int ancestor = -1;		
+		@SuppressWarnings("unused")
+		Graph graph = null;
+		
+		BreadthFirstDirectedPaths bfsV = new BreadthFirstDirectedPaths(digraph, v);
+		BreadthFirstDirectedPaths bfsW = new BreadthFirstDirectedPaths(digraph, w);
+
+		Stack<Integer> stackV = new Stack<>();
+		for (int i : bfsV.pathTo(0))	{stackV.push(i);}	
+		Stack<Integer> stackW = new Stack<>();
+		for (int i : bfsW.pathTo(0))	{stackW.push(i);}
+		
+		for (int i : stackV){
+			if (stackV.peek() == stackW.peek()){
+				ancestor = stackV.peek();
+				stackV.pop(); 
+				stackW.pop();
+			}
+		}
+		
+		StdOut.print("\nancestor: "+ancestor);
+		StdOut.print("\n");	
+		
+
+		
+//TODO: delete unused----------------------------------------
+//		StdOut.print("\nstackV:\n");
+//		StdOut.print(stackV.toString());
+//		StdOut.print("\n");
+//		StdOut.print("stackW:\n");
+//		StdOut.print(stackW.toString());
+//		StdOut.print("\n");	
+//				if (stackV.peek() != null){
+//					graph.addEdge(stackV.pop(), stackV.peek());
+//				}
+//		int vPeek = 0;
+//		int wPeek = 0;
+		//while (!stackV.isEmpty())
+		//for (int i : stackW.pop())
+//			vPeek = stackV.peek();
+//			wPeek = stackW.peek();
+//		Queue<Integer> queueW = new Queue<>();
+//		for (int i : bfsW.pathTo(0))
+//			if (stackV.peek() != i)
+//				queueW.enqueue(stackV.pop());
+		
+//		for (int i : bfsW.pathTo(0))
+//			stackW.push(i); wlength++;
+		
+//		Queue<Integer> queueV = new Queue<>();
+//		for (int i : bfsV.pathTo(0)) 
+//			queueV.enqueue(i);
+//		StdOut.print("\nbfsV:\n");
+//		for (int i : bfsV.pathTo(0)){
+//			StdOut.print(i);
+//			if (i != 0) StdOut.print(" > ");
+//		}
+//		StdOut.print("\nbfsW:\n");
+//		for (int i : bfsW.pathTo(0)){
+//			StdOut.print(i);
+//			if (i != 0) StdOut.print(" > ");
+//		}
+//		StdOut.print("\nqueueV:\n");
+//		StdOut.print(queueV.toString());																								
+//		StdOut.print("\nqueueW:\n");
+//		StdOut.print(queueW.toString());
+																								
+
+//end trace--------------------------------------------------
+		return ancestor;
+	}
+
+	/**TODO
+	 *  a common ancestor that participates in shortest ancestral path; -1 if no such path
+	 * @param v
+	 * @param w
+	 * @return
+	 */
+	//public int ancestor(int v, int w){
+	public int ancestor(Iterable<Integer> v, Iterable<Integer> w){
+		
+		return 0;
+	}
+	
+	/**TODO
+	 *  length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
+	 * @param v
+	 * @param w
+	 * @return
+	 */
+	public int length(Iterable<Integer> v, Iterable<Integer> w){
+		
+		return 0;
+	}
+	
+
+	/**TODO
+	 *  do unit testing of this class
+	 * @param args
+	 */
+	public static void main(String[] args){
+		
+	}
+
+//TODO: delete testing functions----------------------------------------
+//	public Topological getTopo() {
+//		return topo;
+//	}
+	
+    public BreadthFirstDirectedPaths getBfs(int source) {
+		BreadthFirstDirectedPaths bfs;
+		return bfs = new BreadthFirstDirectedPaths(digraph, source);
+	}
+}
--- tests/synsets/SAPTest.java
+++ tests/synsets/SAPTest.java
@@ -6,31 +6,102 @@ import static org.junit.Assert.fail;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import edu.princeton.cs.algs4.BreadthFirstDirectedPaths;
 import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.DirectedEdge;
+import edu.princeton.cs.algs4.Topological;
 import edu.princeton.cs.introcs.In;
 import edu.princeton.cs.introcs.StdOut;
-
+@SuppressWarnings("unused")
 public class SAPTest {
 
 	private In inDigraph1 = new In("/wordnetTests/digraph1.txt");
 	Digraph digraph1 = new Digraph(inDigraph1);
 	private SAP SAPdigraph1 = new SAP(digraph1);
 	
+	private In inDigraph4 = new In("/wordnetTests/digraph4.txt");
+	Digraph digraph4 = new Digraph(inDigraph4);
+	private SAP SAPdigraph4 = new SAP(digraph4);
+	
+	private In inDigraph6 = new In("/wordnetTests/digraph6.txt");
+	Digraph digraph6 = new Digraph(inDigraph6);
+	private SAP SAPdigraph6 = new SAP(digraph6);
+	
+	private In inSapEx1 = new In("/wordnetTests/sapEx1.txt");
+	Digraph sapEx1 = new Digraph(inSapEx1);
+	private SAP SAPsapEx1 = new SAP(sapEx1);
+	
+	private In inSapEx2 = new In("/wordnetTests/sapEx2.txt");
+	Digraph sapEx2 = new Digraph(inSapEx2);
+	private SAP SAPsapEx2 = new SAP(sapEx2);
+	
 	private In inDigraphCycle = new In("/wordnetTests/digraphCycle.txt");
 	Digraph digraphCycle = new Digraph(inDigraphCycle);
 	private SAP SAPdigraphCycle = new SAP(digraphCycle);
 	
+	
+	/************************************************
+	 * Basic construction
+	 ***********************************************/
 	@Test
 	public void testSAP() {
-		assertEquals(true, SAPdigraph1 != null);
+		assertEquals("it's nothing", true, SAPdigraph1 != null);
+//		trace(sapEx1, "SAPsapEx1");
+//		trace(SAPsapEx1.getBfs(3), "SAPsapEx1 bfs 3");
+//		trace(SAPsapEx1.getBfs(11), "SAPsapEx1 bfs 11");
+//		trace(sapEx2, "SAPsapEx2");
+//		trace(SAPsapEx2.getBfs(1), "SAPsapEx2 bfs 1");
+//		trace(SAPsapEx2.getBfs(5), "SAPsapEx2 bfs 5");
+		
+//		trace(digraph1, "digraph1");
+//		trace(digraph4, "digraph4");
+//		trace(digraph6, "digraph6");
+//		trace(SAPsapEx1.getTopo(), "SAPsapEx1 topo");
+//		trace(SAPsapEx2.getTopo(), "SAPsapEx2 topo");
+//		trace(SAPdigraph1.getTopo(), "digraph1 topo");
+//		trace(SAPdigraph4.getTopo(), "digraph4 topo");
+//		trace(SAPdigraph6.getTopo(), "digraph6 topo");
 	}	
-
 	@Test
 	public void testIsDAG() {
-		assertEquals(false, SAPdigraph1.isDAG());
-		assertEquals(true, SAPdigraphCycle.isDAG());
+		assertEquals("Has a cycle", true, SAPdigraph1.isDAG());
+		assertEquals(false, SAPdigraphCycle.isDAG());
+//		trace(digraphCycle, "digraphCycle");
 	}
-
+	@Test
+	public void testAncestorIntInt() {
+		StdOut.print("\nSAPsapEx1 3,11");
+		assertEquals(1, SAPsapEx1.ancestor(3, 11));
+		StdOut.print("\nSAPsapEx1 4,6");
+		assertEquals(0, SAPsapEx1.ancestor(4, 6));
+		StdOut.print("\nSAPsapEx1 7,8");
+		assertEquals(3, SAPsapEx1.ancestor(7, 8));
+		StdOut.print("\nSAPsapEx1 9,12");
+		assertEquals(5, SAPsapEx1.ancestor(9, 12));
+		
+		StdOut.print("\nSAPsapEx2 1,5");
+		assertEquals(0, SAPsapEx2.ancestor(1, 5));
+		
+//		SAPsapEx1.ancestor(4, 6);
+//		SAPsapEx1.ancestor(3, 11);
+//		SAPsapEx2.ancestor(1, 5);
+	}
+	
+	
+	/************************************************
+	 * Traces and analysis
+	 ***********************************************/
+	private static void trace(Digraph graph, String name){
+		StdOut.print("\n\n"+name+"-------------\n"+graph.toString());
+	}
+	private static void trace(Topological topo, String name){
+		StdOut.print(""+name+"-------------\n"+topo.order()+"\n");
+	}
+	private static void trace(BreadthFirstDirectedPaths bfs, String name){
+		StdOut.print(""+name+"-------------\n"+bfs.pathTo(0).toString()+"\n");
+	}
+	
+	
 	@Ignore
 	public void testIsRootedDAG() {
 		fail("Not yet implemented");
@@ -41,10 +112,7 @@ public class SAPTest {
 		fail("Not yet implemented");
 	}
 
-	@Ignore
-	public void testAncestorIntInt() {
-		fail("Not yet implemented");
-	}
+
 
 	@Ignore
 	public void testLengthIterableOfIntegerIterableOfInteger() {
